3aa295a4521ba0f90355859ce6f3f0c992c8213e
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-05-15 07:40:55 +0900

    #6

--- PixivFilter/content_script.js
+++ PixivFilter/content_script.js
@@ -1,3 +1,4 @@
+//NG登録したユーザーのイラストを非表示
 const deleteElement = (userId = String()) => {
     const elements = document.querySelectorAll(".l7cibp-2.mHtZd");
     //console.log(elements);
@@ -11,38 +12,51 @@ const deleteElement = (userId = String()) => {
     })
 }
 
-const createAddElement = async (elements = []) => {
-    return Array.prototype.map.call((elements), (element) => {
-        try {
-            element.querySelector('.sc-1rx6dmq-2.cjMwiA').insertAdjacentHTML("afterend", '<div class="addButton"> [+]</div>');
-        } catch (error) {
-            ;
-        }
-        return
+//ユーザー名の隣にNG登録するボタンを設置
+const createAddElement = async (elements = []) => new Promise(async (resolve) => {
+    Array.prototype.map.call((elements), (element, index) => {
+        const interval = setInterval(() => {
+            const target = element.getElementsByClassName('sc-1rx6dmq-0 jMjLmW');
+            if (target) {
+                console.log("ループ確認用", index)
+                clearInterval(interval);
+                target[0].insertAdjacentHTML("afterend", '<div class="addButton"> [+]</div>');
+                //すべての要素に追加してからresolveする
+                if (elements.length - 1 === index) { resolve(); }
+            };
+        }, 500);
     });
-}
+});
 
-const addToStorage = () => {
+//NG登録ボタンを押したらChromeストレージに保存する
+const addToStorage = async () => {
     const targets = document.querySelectorAll('.addButton');
-    //let userIds = [];
     Array.prototype.map.call((targets), (target) => {
+        console.log(target);
         target.addEventListener('click', async (e) => {
+            e.stopPropagation();
+            const userName = e.path[1].querySelector('[title]').getAttribute("title");
             const userId = e.path[1].querySelector('[href]').getAttribute("href").slice(7);
-            console.log(userId);
+            console.log(userName, userId);
+            const userInfo = { userName: userName, userId: userId };
+            //console.log(userId);
             //if (checkLocalStorage('userId')[0] === undefined) { }
             //userIds = ;
-            let userIds = await checkLocalStorage('userId');
-            console.log(userIds);
-            userIds.push(userId)
+            //let userIds = await checkLocalStorage('userId');
+            //console.log(userIds);
+            //userIds.push(userId)
             //console.log(userId);
-            chrome.storage.sync.set({ userId: userIds }, () => { return })
-            deleteElement(userId);
+            //chrome.storage.sync.set({ userInfo: [userInfo] }, () => { return })
+            //chrome.storage.sync.get(["userInfo"], (result) => { console.log(result); })
+            return
+            //deleteElement(userId);
             //console.log(e);
 
-        });
-    })
-}
+        }, false);
+    });
+};
 
+//ChromeストレージにNGユーザーが登録されているかを確認
 const checkLocalStorage = (query = String()) => new Promise((resolve) => {
     chrome.storage.sync.get([query], (dic) => {
         const results = dic.userId;
@@ -57,21 +71,22 @@ const checkLocalStorage = (query = String()) => new Promise((resolve) => {
     });
 });
 
-const main = () => {
+const main = async () => {
     const interval = setInterval(async () => {
-        const elements = document.querySelectorAll(".l7cibp-2.mHtZd");
-        console.log(elements)
-        if (elements.length) {
+        const elements = document.getElementsByClassName("l7cibp-2 mHtZd");
+        if (elements[0]) {
+            console.log(elements);
             clearInterval(interval);
+            console.log("test");
             await createAddElement(elements);
-            await checkLocalStorage("userId")
+            console.log("test");
+            await addToStorage();
+            //await checkLocalStorage("userId")
             //deleteElement("89795682");
             //deleteElement("68475181");
             //chrome.storage.sync.clear()
-
-            addToStorage();
         }
     }, 1000);
-}
+};
 
 document.addEventListener('load', main());
