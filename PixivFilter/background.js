0f9480fbc469da0637412b32e2f7173a9287c9a6
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-05-19 20:47:33 +0900

    #48

--- PixivFilter/background.js
+++ PixivFilter/background.js
@@ -7,37 +7,92 @@ const getTabId = () => new Promise((resolve, reject) => {
 });
 
 let count = 0;
+
 chrome.webRequest.onBeforeRequest.addListener(
     async (e) => {
         //console.log(e);
         const url = e.url;
+        console.log(url)
+        /*
         if (url.includes('https://www.pixiv.net/ajax/top/')) {
             console.log(url);
             chrome.tabs.sendMessage(await getTabId(), "");
         }
+        */
 
-        if (url.includes('https://www.pixiv.net/ajax/search/artworks/') && count === 0) {
-            count += 1;
-            console.log(count);
+        const searchTargets = [
+            "https://www.pixiv.net/ajax/search/illustrations/",
+            "https://www.pixiv.net/ajax/search/artworks/",
+            "https://www.pixiv.net/ajax/search/manga/",
+            "https://www.pixiv.net/ajax/search/top/"
+        ];
+        await Promise.all(searchTargets.map(async (searchTarget) => {
+            if (url.includes(searchTarget) && count === 0) {
+                count += 1;
+                console.log(count);
 
-            const json = await fetch(url).then((res) => { return (res.json()); });
-            console.log(json);
+                const json = await fetch(url).then((res) => { return (res.json()); });
+                console.log(json);
 
-            let illustDatas = json.body.illustManga.data;
-
-            illustDatas = await Promise.all(illustDatas.map((illustData) => {
-                const illustId = illustData.id;
-                const tags = illustData.tags;
-                if (illustId) {
-                    return { illustId: illustId, tags: tags };
+                let illustDatas = [];
+                if (searchTarget === 'https://www.pixiv.net/ajax/search/artworks/' || searchTarget === 'https://www.pixiv.net/ajax/search/top/') {
+                    illustDatas = json.body.illustManga.data;
+                } else if (searchTarget === 'https://www.pixiv.net/ajax/search/illustrations/') {
+                    illustDatas = json.body.illust.data;
+                } else if (searchTarget === 'https://www.pixiv.net/ajax/search/manga/') {
+                    illustDatas = json.body.manga.data;
                 };
-            }));
-            illustDatas = illustDatas.filter((n) => { return n != undefined });
-            console.log(illustDatas);
-            //{illustId:"",tags:[]}
-            chrome.tabs.sendMessage(await getTabId(), illustDatas);
-        } else { count = 0; };
+
+                illustDatas = await Promise.all(illustDatas.map((illustData) => {
+                    const illustId = illustData.id;
+                    const tags = illustData.tags;
+                    if (illustId) {
+                        return { illustId: illustId, tags: tags };
+                    };
+                }));
+                illustDatas = illustDatas.filter((n) => { return n != undefined });
+                console.log(illustDatas);
+
+                //chrome.storage.local.set({ illustDatas: illustDatas });
+
+                //chrome.storage.local.get(null, (result) => { console.log(result); });
+                //{illustId:"",tags:[]}
+                //chrome.tabs.executeScript({ file: 'content_script.js' });
+                chrome.tabs.sendMessage(await getTabId(), illustDatas);
+            } /*else { count = 0; }*/;
+
+        }));
+        count = 0;
     },
     { urls: ['*://www.pixiv.net/*'] },
     ['requestBody', 'blocking']
 );
+
+/*
+let count = 0;
+const tabStatus = async (tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete" && tab.url.includes("https://www.pixiv.net/tags/") && count === 0) {
+        count += 1;
+        console.log(tabId, changeInfo, tab);
+        console.log(tab.url);
+
+        const json = await fetch(url).then((res) => { return (res.json()); });
+        console.log(json);
+
+        let illustDatas = json.body.illustManga.data;
+
+        illustDatas = await Promise.all(illustDatas.map((illustData) => {
+            const illustId = illustData.id;
+            const tags = illustData.tags;
+            if (illustId) {
+                return { illustId: illustId, tags: tags };
+            };
+        }));
+        illustDatas = illustDatas.filter((n) => { return n != undefined });
+        console.log(illustDatas);
+        chrome.tabs.sendMessage(await getTabId(), illustDatas);
+    };
+};
+
+chrome.tabs.onUpdated.addListener(tabStatus);
+*/
