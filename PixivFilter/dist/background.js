8afe08d5b0cd2701bc8b84c0cd40ad9bea01ee26
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-12-05 05:46:00 +0900

    feat: eslintを導入した

--- PixivFilter/dist/background.js
+++ PixivFilter/dist/background.js
@@ -1,55 +1,32 @@
-const getTabId = () => new Promise((resolve, reject) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
-        const tabId = tab[0].id
-        console.log(tabId);
-        resolve(tabId);
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-});
-
+};
 let count = 0;
-
-chrome.webRequest.onBeforeRequest.addListener(
-    async (e) => {
-        const url = e.url;
-        const searchTargets = [
-            "https://www.pixiv.net/ajax/search/illustrations/",
-            "https://www.pixiv.net/ajax/search/artworks/",
-            "https://www.pixiv.net/ajax/search/manga/",
-            "https://www.pixiv.net/ajax/search/top/"
-        ];
-        await Promise.all(searchTargets.map(async (searchTarget) => {
-            if (url.includes(searchTarget) && count === 0) {
-                count += 1;
-                console.log(count);
-
-                const json = await fetch(url).then((res) => { return (res.json()); });
-                console.log(json);
-
-                let illustDatas = [];
-                if (searchTarget === 'https://www.pixiv.net/ajax/search/artworks/' || searchTarget === 'https://www.pixiv.net/ajax/search/top/') {
-                    illustDatas = json.body.illustManga.data;
-                } else if (searchTarget === 'https://www.pixiv.net/ajax/search/illustrations/') {
-                    illustDatas = json.body.illust.data;
-                } else if (searchTarget === 'https://www.pixiv.net/ajax/search/manga/') {
-                    illustDatas = json.body.manga.data;
-                };
-
-                illustDatas = await Promise.all(illustDatas.map((illustData) => {
-                    const illustId = illustData.id;
-                    const tags = illustData.tags;
-                    if (illustId) {
-                        return { illustId: illustId, tags: tags };
-                    };
-                }));
-                illustDatas = illustDatas.filter((n) => { return n != undefined });
-                console.log(illustDatas);
-
-                chrome.tabs.sendMessage(await getTabId(), illustDatas);
-            };
-
-        }));
-        count = 0;
-    },
-    { urls: ['*://www.pixiv.net/*'] },
-    ['requestBody', 'blocking']
-);
+function getIllusts(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield (yield fetch(url)).json();
+        console.log(response.body);
+    });
+}
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    var _a;
+    if (changeInfo.status === 'complete' &&
+        ((_a = tab.url) === null || _a === void 0 ? void 0 : _a.includes('https://www.pixiv.net/tags/'))) {
+        //  console.log({ tabId, changeInfo, tab });
+    }
+});
+chrome.webRequest.onBeforeRequest.addListener((details) => {
+    if (details.url.includes('https://www.pixiv.net/ajax/search/') &&
+        details.initiator === 'https://www.pixiv.net') {
+        count += 1;
+        getIllusts(details.url);
+        console.log(details);
+    }
+}, { urls: ['https://www.pixiv.net/*'] });
--- PixivFilter/src/background.ts
+++ PixivFilter/src/background.ts
@@ -1,55 +1,144 @@
-const getTabId = () => new Promise((resolve, reject) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
-        const tabId = tab[0].id
-        console.log(tabId);
-        resolve(tabId);
-    });
-});
+interface illustsJSON {
+  error: boolean;
+  body: Body;
+}
+
+interface Body {
+  novel: Novel;
+  popular: Popular;
+  relatedTags: string[];
+  tagTranslation: any[];
+  zoneConfig: ZoneConfig;
+  extraData: ExtraData;
+  illust: Illust;
+  manga: Illust;
+}
+
+interface ExtraData {
+  meta: Meta;
+}
+
+interface Meta {
+  title: string;
+  description: string;
+  canonical: string;
+  alternateLanguages: AlternateLanguages;
+  descriptionHeader: string;
+}
+
+interface AlternateLanguages {
+  ja: string;
+  en: string;
+}
+
+interface Illust {
+  data: PermanentElement[];
+  total: number;
+}
+
+interface PermanentElement {
+  id: string;
+  title: string;
+  illustType: number;
+  xRestrict: number;
+  restrict: number;
+  sl: number;
+  url: string;
+  description: string;
+  tags: string[];
+  userId: string;
+  userName: string;
+  width: number;
+  height: number;
+  pageCount: number;
+  isBookmarkable: boolean;
+  bookmarkData: null;
+  alt: string;
+  titleCaptionTranslation: TitleCaptionTranslation;
+  createDate: Date;
+  updateDate: Date;
+  isUnlisted: boolean;
+  isMasked: boolean;
+  profileImageUrl: string;
+}
+
+interface TitleCaptionTranslation {
+  workTitle: null;
+  workCaption: null;
+}
+
+interface Novel {
+  data: NovelDatum[];
+  total: number;
+}
+
+interface NovelDatum {
+  id: string;
+  title: string;
+  xRestrict: number;
+  restrict: number;
+  url: string;
+  tags: string[];
+  userId: string;
+  userName: string;
+  profileImageUrl: string;
+  textCount: number;
+  description: string;
+  isBookmarkable: boolean;
+  bookmarkData: null;
+  bookmarkCount: number;
+  isOriginal: boolean;
+  marker: null;
+  titleCaptionTranslation: TitleCaptionTranslation;
+  createDate: Date;
+  updateDate: Date;
+  isMasked: boolean;
+  seriesId?: string;
+  seriesTitle?: string;
+  isUnlisted: boolean;
+}
+
+interface Popular {
+  recent: PermanentElement[];
+  permanent: PermanentElement[];
+}
+
+interface ZoneConfig {
+  header: Footer;
+  footer: Footer;
+  infeed: Footer;
+}
+
+interface Footer {
+  url: string;
+}
 
 let count = 0;
+async function getIllusts(url: string) {
+  const response: illustsJSON = await (await fetch(url)).json();
+
+  console.log(response.body);
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (
+    changeInfo.status === 'complete' &&
+    tab.url?.includes('https://www.pixiv.net/tags/')
+  ) {
+    //  console.log({ tabId, changeInfo, tab });
+  }
+});
 
 chrome.webRequest.onBeforeRequest.addListener(
-    async (e) => {
-        const url = e.url;
-        const searchTargets = [
-            "https://www.pixiv.net/ajax/search/illustrations/",
-            "https://www.pixiv.net/ajax/search/artworks/",
-            "https://www.pixiv.net/ajax/search/manga/",
-            "https://www.pixiv.net/ajax/search/top/"
-        ];
-        await Promise.all(searchTargets.map(async (searchTarget) => {
-            if (url.includes(searchTarget) && count === 0) {
-                count += 1;
-                console.log(count);
-
-                const json = await fetch(url).then((res) => { return (res.json()); });
-                console.log(json);
-
-                let illustDatas = [];
-                if (searchTarget === 'https://www.pixiv.net/ajax/search/artworks/' || searchTarget === 'https://www.pixiv.net/ajax/search/top/') {
-                    illustDatas = json.body.illustManga.data;
-                } else if (searchTarget === 'https://www.pixiv.net/ajax/search/illustrations/') {
-                    illustDatas = json.body.illust.data;
-                } else if (searchTarget === 'https://www.pixiv.net/ajax/search/manga/') {
-                    illustDatas = json.body.manga.data;
-                };
-
-                illustDatas = await Promise.all(illustDatas.map((illustData) => {
-                    const illustId = illustData.id;
-                    const tags = illustData.tags;
-                    if (illustId) {
-                        return { illustId: illustId, tags: tags };
-                    };
-                }));
-                illustDatas = illustDatas.filter((n) => { return n != undefined });
-                console.log(illustDatas);
-
-                chrome.tabs.sendMessage(await getTabId(), illustDatas);
-            };
-
-        }));
-        count = 0;
-    },
-    { urls: ['*://www.pixiv.net/*'] },
-    ['requestBody', 'blocking']
+  (details) => {
+    if (
+      details.url.includes('https://www.pixiv.net/ajax/search/') &&
+      details.initiator === 'https://www.pixiv.net'
+    ) {
+      count += 1;
+      getIllusts(details.url);
+      console.log(details);
+    }
+  },
+  { urls: ['https://www.pixiv.net/*'] }
 );
