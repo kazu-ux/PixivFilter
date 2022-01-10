4374a6a4868dcba1303ed7f87f85ef56dbbefa36
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2022-01-10 20:33:48 +0900

    update: 広告を除外した

--- src/fetch_api.ts
+++ src/fetch_api.ts
@@ -1,4 +1,4 @@
-export const getRequest = async (url: string, pages: string) => {
+export const getRequest = async (url: string, pages?: string) => {
   const getWorksData = (json: any) => {
     let worksData: WorksData = [];
     try {
@@ -16,13 +16,33 @@ export const getRequest = async (url: string, pages: string) => {
     } catch (error) {}
     return worksData;
   };
+  if (!pages) {
+    const json = await (await fetch(url)).json();
+    const worksData = getWorksData(json)!
+      .filter(Boolean)
+      .flatMap((data) => {
+        if (Object.keys(data).includes('isAdContainer')) {
+          return [];
+        }
+        return [data];
+      });
+
+    return worksData;
+  }
 
   const newURL = new URL(url);
   newURL.searchParams.set('p', pages);
   const href = newURL.href;
   console.log(href);
   const json = await (await fetch(href)).json();
-  const worksData = getWorksData(json)!.filter(Boolean);
+  const worksData = getWorksData(json)!
+    .filter(Boolean)
+    .flatMap((data) => {
+      if (Object.keys(data).includes('isAdContainer')) {
+        return [];
+      }
+      return [data];
+    });
 
   return worksData;
 };
