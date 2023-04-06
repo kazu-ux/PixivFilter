a4b8daf6bd7f3a4ce0388deb6c296821e905245c
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2023-04-06 19:52:47 +0900

    clean: リファクタリング

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,4 @@
 import { ChromeStorage } from './database/chrome_storage';
-import MigrateStorage from './migrate_storage';
 
 const fetchWork = async (url: string) => {
   const isRequest = await ChromeStorage.getRequestFlag();
@@ -33,113 +32,96 @@ const fetchWork = async (url: string) => {
   }
 };
 
-/* (async () => {
-  MigrateStorage();
-  await ChromeStorage.setRequestFlag(false);
+const callback = async (res: chrome.webRequest.WebResponseCacheDetails) => {
+  if (res.initiator === 'https://www.pixiv.net') {
+    const keyword = res.url.split('/')[5].split('?')[0];
+    console.log(keyword);
 
-  const test = ChromeStorage.getUser();
-  if (test) {
-    test.then((res) => {
-      console.log(res);
-    });
+    console.log(res.url);
+
+    const getWorks: { [key: string]: () => Promise<object[]> } = {
+      top: async () => {
+        const json: SearchTop = await fetchWork(res.url);
+
+        return [
+          json.body.illustManga?.data ?? {},
+          json.body.novel?.data ?? {},
+          json.body.popular?.recent ?? {},
+          json.body.popular?.permanent ?? {},
+        ].flat();
+      },
+      artworks: async () => {
+        const json: Artworks = await fetchWork(res.url);
+
+        return [
+          json.body.illustManga?.data ?? {},
+          json.body.popular?.recent ?? {},
+          json.body.popular?.permanent ?? {},
+        ].flat();
+      },
+      illust: async () => {
+        const json: Illustrations = await fetchWork(res.url);
+
+        return [
+          json.body.thumbnails?.illust ?? {},
+          json.body.thumbnails?.novel ?? {},
+        ].flat();
+      },
+      illustrations: async () => {
+        const json: Illustrations = await fetchWork(res.url);
+
+        return [
+          json.body.illust?.data ?? {},
+          json.body.popular?.recent ?? {},
+          json.body.popular?.permanent ?? {},
+        ].flat();
+      },
+      manga: async () => {
+        const json: Manga = await fetchWork(res.url);
+
+        return [
+          json.body.thumbnails?.illust ?? {},
+          json.body.thumbnails?.novel ?? {},
+          json.body.manga?.data ?? {},
+          json.body.popular?.recent ?? {},
+          json.body.popular?.permanent ?? {},
+        ].flat();
+      },
+      novels: async () => {
+        const json: Novels = await fetchWork(res.url);
+        return [json.body.novel?.data ?? {}].flat();
+      },
+      novel: async () => {
+        const json: Novels = await fetchWork(res.url);
+        return [
+          json.body.novel?.data ?? {},
+          json.body.thumbnails?.illust ?? {},
+
+          json.body.thumbnails?.novel ?? {},
+          json.body.thumbnails?.novelSeries ?? {},
+        ].flat();
+      },
+    };
+
+    const keywords = Object.keys(getWorks);
+    if (!keywords.includes(keyword)) return;
+
+    const worksData = (await getWorks[keyword]()) as WorksData;
+
+    await ChromeStorage.setWorksData(worksData);
+
+    console.log(worksData);
+
+    console.log(res);
+    console.log(res.initiator);
+    return;
   }
-})(); */
 
-/* chrome.runtime.onMessage.addListener((res) => {
-  console.log(res);
-}); */
+  console.log('ピクシブからのアクセスではない');
+};
 
-chrome.webRequest.onCompleted.addListener(
-  async (res) => {
-    if (res.initiator === 'https://www.pixiv.net') {
-      const keyword = res.url.split('/')[5].split('?')[0];
-      console.log(keyword);
-
-      console.log(res.url);
-
-      const getWorks: { [key: string]: () => Promise<object[]> } = {
-        top: async () => {
-          const json: SearchTop = await fetchWork(res.url);
-
-          return [
-            json.body.illustManga?.data ?? {},
-            json.body.novel?.data ?? {},
-            json.body.popular?.recent ?? {},
-            json.body.popular?.permanent ?? {},
-          ].flat();
-        },
-        artworks: async () => {
-          const json: Artworks = await fetchWork(res.url);
-
-          return [
-            json.body.illustManga?.data ?? {},
-            json.body.popular?.recent ?? {},
-            json.body.popular?.permanent ?? {},
-          ].flat();
-        },
-        illust: async () => {
-          const json: Illustrations = await fetchWork(res.url);
-
-          return [
-            json.body.thumbnails?.illust ?? {},
-            json.body.thumbnails?.novel ?? {},
-          ].flat();
-        },
-        illustrations: async () => {
-          const json: Illustrations = await fetchWork(res.url);
-
-          return [
-            json.body.illust?.data ?? {},
-            json.body.popular?.recent ?? {},
-            json.body.popular?.permanent ?? {},
-          ].flat();
-        },
-        manga: async () => {
-          const json: Manga = await fetchWork(res.url);
-
-          return [
-            json.body.thumbnails?.illust ?? {},
-            json.body.thumbnails?.novel ?? {},
-            json.body.manga?.data ?? {},
-            json.body.popular?.recent ?? {},
-            json.body.popular?.permanent ?? {},
-          ].flat();
-        },
-        novels: async () => {
-          const json: Novels = await fetchWork(res.url);
-          return [json.body.novel?.data ?? {}].flat();
-        },
-        novel: async () => {
-          const json: Novels = await fetchWork(res.url);
-          return [
-            json.body.novel?.data ?? {},
-            json.body.thumbnails?.illust ?? {},
-
-            json.body.thumbnails?.novel ?? {},
-            json.body.thumbnails?.novelSeries ?? {},
-          ].flat();
-        },
-      };
-
-      const keywords = Object.keys(getWorks);
-      if (!keywords.includes(keyword)) return;
-
-      const worksData = (await getWorks[keyword]()) as WorksData;
-
-      await ChromeStorage.setWorksData(worksData);
-
-      console.log(worksData);
-
-      console.log(res);
-      console.log(res.initiator);
-      return;
-    }
-
-    console.log('ピクシブからのアクセスではない');
-  },
-  {
-    urls: ['https://www.pixiv.net/ajax/search/*'],
-  }
-);
+chrome.webRequest.onCompleted.addListener(callback, {
+  urls: ['https://www.pixiv.net/ajax/search/*'],
+});
 
 export {};
